5380f88ae12c7460aa7b956c2fc3619c665826c8
odyarapu-krupakar <vodyarapukrupakar80@gmail.com>
2026-08-12 11:56:17 +0530

    hello  krupakar

---
+++ core_java/src/Krupakar2.java
@@ -0,0 +1,12 @@
+
+public class Krupakar2 {
+	public static void main(String[]args) {
+		System.out.println("hello krupakar ");
+		System.out.println("hello krupakar ");
+		System.out.println("hello krupakar ");
+		System.out.println("hello krupakar ");
+		System.out.println("hello krupakar ");
+		
+	}
+
+}
